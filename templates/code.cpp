3b0cf65c22c5ef5eed4f74f22c167583cec9acef
Michael Bartling <michael.bartling@arm.com>
2017-11-29 14:25:08 -0600

    Add example workflow

--- templates/code.cpp
+++ templates/code.cpp
@@ -12,7 +12,7 @@ namespace {{ name }} {
     }
 
     S_TENSOR Model::predict_proba({% for i in range(num_inputs-1) %}S_TENSOR input{{i}}, {% endfor %}S_TENSOR input{{ num_inputs-1 }}){
-        {% for i in range(num_inputs) %} ctx.add(input{{i}}, "input{{i}}");{% endfor %}
+        {% for i in range(num_inputs) %}ctx.add(input{{i}}, "input{{i}}");{% endfor %}
 
         inference(ctx);
         return ctx.get("output");
